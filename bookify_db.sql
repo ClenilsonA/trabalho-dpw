6c9077a755cf174f80b0b445505f13c8436228ff
ClenilsonA <clenia003@gmail.com>
2026-01-22 11:54:38 +0000

    Add files via upload

--- bookify_db.sql
+++ bookify_db.sql
@@ -14,6 +14,26 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for bookify_db
+DROP DATABASE IF EXISTS `bookify_db`;
+CREATE DATABASE IF NOT EXISTS `bookify_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bookify_db`;
+
+-- Dumping structure for table bookify_db.livros
+DROP TABLE IF EXISTS `livros`;
+CREATE TABLE IF NOT EXISTS `livros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `sinopse` text,
+  `capa_url` varchar(255) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
+  `is_popular` tinyint(1) NOT NULL,
+  `conteudo_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1008 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Dumping data for table bookify_db.livros: ~7 rows (approximately)
 INSERT INTO `livros` (`id`, `titulo`, `autor`, `sinopse`, `capa_url`, `categoria`, `is_popular`, `conteudo_url`) VALUES
 	(1001, 'Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 'An orphaned boy discovers he is a wizard and attends a magical school, beginning a series of adventures against a dark lord.', 'assets\\images\\Vector.png', 'Fantasy', 1, 'livros'),
@@ -24,9 +44,34 @@ INSERT INTO `livros` (`id`, `titulo`, `autor`, `sinopse`, `capa_url`, `categoria
 	(1006, 'The Lord of the Rings', 'J.R.R. Tolkien', 'A young hobbit inherits a powerful, corrupted ring and must embark on a quest across Middle-earth to destroy it, facing the Dark Lord Sauron.', 'assets\\images\\tlor.jpg', 'Fantasy', 1, NULL),
 	(1007, 'The Da Vinci Code', 'Dan Brown', 'A Harvard professor races across Europe to solve a series of cryptic clues tied to Da Vinci\'s work and a secret society protecting a historical secret.', 'assets\\images\\davinchcode.jpg', 'Thriller', 1, NULL);
 
+-- Dumping structure for table bookify_db.minha_lista
+DROP TABLE IF EXISTS `minha_lista`;
+CREATE TABLE IF NOT EXISTS `minha_lista` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_utilizadores` int NOT NULL,
+  `id_livros` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_user_book` (`id_utilizadores`,`id_livros`),
+  KEY `fk_books` (`id_livros`),
+  CONSTRAINT `fk_books` FOREIGN KEY (`id_livros`) REFERENCES `livros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_user` FOREIGN KEY (`id_utilizadores`) REFERENCES `utilizadores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 -- Dumping data for table bookify_db.minha_lista: ~0 rows (approximately)
 
--- Dumping data for table bookify_db.utilizadores: ~0 rows (approximately)
+-- Dumping structure for table bookify_db.utilizadores
+DROP TABLE IF EXISTS `utilizadores`;
+CREATE TABLE IF NOT EXISTS `utilizadores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `data_registo` timestamp NOT NULL DEFAULT (now()),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table bookify_db.utilizadores: ~3 rows (approximately)
 INSERT INTO `utilizadores` (`id`, `nome`, `email`, `senha`, `data_registo`) VALUES
 	(1, 'Kessyane', 'kessyaneloureiro@gmail.com', '$2y$10$q3LTttWsgCPS2Jxgtak53eoHRbaSYBbsVNlCDu7DsPfM6wIlu3aFq', '2025-12-16 14:52:36'),
 	(2, 'Jean', 'jean@gmail.com', '$2y$10$lesMv.h0aPTfZSX9mgZwSuunjwA4kAhk5ZvzFFLJd.1SI0naj6F5O', '2026-01-20 23:20:44'),
